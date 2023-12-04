5b0f5ee7ab73b5691d045046d85df3f33e24c844
zbyju <zbynek.jurica@gmail.com>
2023-12-04 14:40:06 +0100

    Add day 4 solution

--- 2023/src/days/day04/solution.rs
+++ 2023/src/days/day04/solution.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::days::{AdventDay, SolutionOutput};
 
 pub struct Day04;
@@ -8,12 +10,90 @@ impl AdventDay for Day04 {
     }
 
     fn part1(&self, input: &str) -> SolutionOutput {
-        println!("Input for Part 1:\n{}", input);
-        SolutionOutput::String("Part 1 output displayed".to_string())
+        let cards: Vec<(Vec<i64>, Vec<i64>)> = input
+            .lines()
+            .map(|l| {
+                let split = l.split(':').last().unwrap().trim();
+                let mut num_split = split.split('|');
+
+                (
+                    num_split
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .split(' ')
+                        .filter(|x| !x.is_empty())
+                        .map(|x| x.parse::<i64>().unwrap())
+                        .collect(),
+                    num_split
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .split(' ')
+                        .filter(|x| !x.is_empty())
+                        .map(|x| x.parse::<i64>().unwrap())
+                        .collect(),
+                )
+            })
+            .collect();
+
+        let points = cards
+            .iter()
+            .map(|(winning_numbers, numbers)| {
+                numbers
+                    .iter()
+                    .filter(|x| winning_numbers.contains(x))
+                    .count()
+            })
+            .map(|c| if c > 0 { 2_i64.pow(c as u32 - 1) } else { 0 });
+
+        SolutionOutput::Int(points.sum())
     }
 
     fn part2(&self, input: &str) -> SolutionOutput {
-        println!("Input for Part 2:\n{}", input);
-        SolutionOutput::String("Part 2 output displayed".to_string())
+        let cards: Vec<(Vec<i64>, Vec<i64>)> = input
+            .lines()
+            .map(|l| {
+                let split = l.split(':').last().unwrap().trim();
+                let mut num_split = split.split('|');
+
+                (
+                    num_split
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .split(' ')
+                        .filter(|x| !x.is_empty())
+                        .map(|x| x.parse::<i64>().unwrap())
+                        .collect(),
+                    num_split
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .split(' ')
+                        .filter(|x| !x.is_empty())
+                        .map(|x| x.parse::<i64>().unwrap())
+                        .collect(),
+                )
+            })
+            .collect();
+
+        let copies = cards
+            .iter()
+            .map(|(winning_numbers, numbers)| {
+                numbers
+                    .iter()
+                    .filter(|x| winning_numbers.contains(x))
+                    .count()
+            })
+            .enumerate()
+            .fold(HashMap::new(), |mut acc, (i, cnt)| {
+                for n in i + 1..=i + cnt {
+                    acc.insert(n, acc.get(&n).unwrap_or(&1) + acc.get(&i).unwrap_or(&1));
+                }
+                acc
+            });
+
+        SolutionOutput::Int((0..cards.len()).map(|i| copies.get(&i).unwrap_or(&1)).sum())
     }
 }
