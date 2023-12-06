20dd0b4a8b37f088298784be4b2b9ee1d7d8cbf2
zbyju <zbynek.jurica@gmail.com>
2023-12-06 11:25:25 +0100

    Optimize day 6 solution to use quadratic formula

--- 2023/src/days/day06/solution.rs
+++ 2023/src/days/day06/solution.rs
@@ -27,24 +27,13 @@ fn solve(input: String) -> SolutionOutput {
     let number_of_ways_to_beat: Vec<i64> = zipped
         .iter()
         .map(|&(time, distance_to_beat)| {
-            let best = time / 2;
-            let mut count = 0;
-
-            for charge_time in (1..best).rev() {
-                if (time - charge_time) * charge_time > distance_to_beat {
-                    count += 1;
-                } else {
-                    break;
-                }
-            }
-            for charge_time in best..time {
-                if (time - charge_time) * charge_time > distance_to_beat {
-                    count += 1;
-                } else {
-                    break;
-                }
-            }
-            count
+            // Quadratic formula (a=1, b=time, c=distance_to_beat)
+            let x = (time - f64::sqrt((time * time - 4 * distance_to_beat) as f64) as i64) / 2;
+            let b = time - x;
+            b - (b * (time - b) <= distance_to_beat) as i64
+                - x
+                - (x * (time - x) <= distance_to_beat) as i64
+                + 1
         })
         .collect();
 
